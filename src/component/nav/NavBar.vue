f4f50b7ce1982fbcade58bf62c2f2288b295224e
HepNuk <nikolaperotic@gmail.com>
2022-01-27 21:51:11 -0500

    NavItems Component

--- src/component/nav/NavBar.vue
+++ src/component/nav/NavBar.vue
@@ -1,15 +1,57 @@
 <template>
-  <div class="nav-bar">
-    I am a NAV BAR
-  </div>
+  <nav class="nav nav-pills gap-1">
+    <li 
+      v-for="navItem in navItems"
+      :key="navItem.value"
+      class="nav-item d-grid"
+    >
+      <!-- For later use -->
+      <!-- <template v-if="navItem.dropDown">
+        <MyButton 
+          :active="navItem.active"
+          @click="$emit('change-view', navItem.value)"
+        >
+          {{ navItem.title }}
+        </MyButton>
+
+        <li style="display: flex; flex-direction: column;" v-if="navItem.active">
+          <MyButton>
+            Test 1
+          </MyButton>
+
+          <MyButton>
+            Test 2
+          </MyButton>
+        </li>
+      </template> -->
+
+      <MyButton 
+        fill
+        :active="navItem.active"
+        @click="$emit('change-view', navItem.value)"
+      >
+        {{ navItem.title }}
+      </MyButton>
+    </li>
+  </nav>
 </template>
 
 <script>
 export default {
+  props: {
+    navItems: {
+      type: Array,
+      required: true,
+    }
+  },
 };
 
 </script>
 
 <style lang="scss" scoped>
-
+.nav {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
 </style>
