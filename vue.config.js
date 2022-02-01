f921a1f8b38390cf7e8a98eebff36824582ed5a3
HepNuk <nikolaperotic@gmail.com>
2022-01-31 21:44:57 -0500

    csv-loader + converted footer into mytable

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 var path = require('path');
 module.exports = {
+  chainWebpack: config => {
+    config
+      .module
+      .rule("csv")
+      .test(/\.csv$/)
+      .use("csv-loader")
+      .loader("csv-loader")
+      .options({
+        dynamicTyping: true,
+        header: true,
+        skipEmptyLines: true
+      })
+      .end();
+  },
   configureWebpack: {
     resolve: {
       alias: {
