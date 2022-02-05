33aa1939560b55acc84691382122c3d6ca683816
HepNuk <nikolaperotic@gmail.com>
2022-02-05 14:36:19 -0500

    Added FortAwesome and CheveronIcons

---
+++ src/fortawesome-icons.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faGripHorizontal } from '@fortawesome/free-solid-svg-icons';
+
+
+library.add(faInfoCircle);
+library.add(faChevronUp);
+library.add(faChevronDown);
+library.add(faGripHorizontal);
+
+export default FontAwesomeIcon;
--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import VuePapaParse from 'vue-papa-parse';
 import VueCsvLoader from "vue-csv-loader";
 import MyButton from './component/shared/MyButton.vue';
 import MyTable from './component/shared/MyTable.vue';
+import FontAwesomeIcon from './fortawesome-icons';
 
 const app = createApp(App);
 
 app.use(VueCsvLoader);
 app.use(VuePapaParse);
+app.component("fa", FontAwesomeIcon);
 app.component("MyButton", MyButton);
 app.component("MyTable", MyTable);
 app.mount('#app');
