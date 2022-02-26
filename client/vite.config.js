0a1df586a9b86d6af93df399c4f9928ea4a87594
HepNuk <nikolaperotic@gmail.com>
2022-02-25 22:54:49 -0500

    public path change

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,6 +9,14 @@ export default defineConfig({
   build: {
     outDir: `${path.resolve(__dirname, '../', 'public')}/`
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/',
+        changeOrigin: true,
+      },
+    }
+  },
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
