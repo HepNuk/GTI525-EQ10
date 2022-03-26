232937b6e0c5c4eac500609ebedbc38fec86a7e7
Ryuketsuki <jimmy.mai.1@ens.etsmtl.ca>
2022-03-25 20:54:27 -0400

    Modification des routes pour inclure les limites

--- server/routes/api/routeCompteur.js
+++ server/routes/api/routeCompteur.js
@@ -1,80 +1,84 @@
-const { Router } = require('express');
+const {Router} = require('express');
 const moment = require('moment');
 const mongoose = require('mongoose');
-const { getCounterModel } = require('../../models/counterModel');
-const { getDataStatsModel } = require('../../models/dataStatsModel');
+const {getCounterModel} = require('../../models/counterModel');
+const {getDataStatsModel} = require('../../models/dataStatsModel');
 
 const router = Router();
 
 // GET /gti525/v1/compteurs
 router.get('/', (req, res) => {
-  const limite = req.query['limite']; // TODO: use limite in request
-  
-  getCounterModel().find({ }, (err, counters) => {
-    res.status(200).send(counters);
-  });
+    const limite = req.query['limite'];
+
+    getCounterModel().find({}, {_id: 0, ID: 1, Nom: 1, Statut: 1, Annee_implante: 1}, (err, counters) => {
+        res.status(200).send(counters);
+    }).limit(limite);
 });
 
 // GET /gti525/v1/compteurs/:compteurId
 router.get('/:id', async (req, res) => {
-  const compteurId = req.params.id;
+    const compteurId = req.params.id;
 
-  await getCounterModel().findOne({ ID: compteurId }).exec((err, result) => {
-    if (err) res.status(400).send(err);
+    await getCounterModel().findOne({ID: compteurId}, {_id: 0, __v: 0}).exec((err, result) => {
+        if (err) res.status(400).send(err);
 
-    res.status(200).send(result);
-  });
+        res.status(200).send(result);
+    });
 });
 
 // GET /gti525/v1/compteurs/:compteurId/passages
-router.get('/:id', async (req, res) => {
-  const compteurId = req.params.id;
-  const start = req.query['debut'];
-  const end = req.query['fin'];
-  const limite = req.query['limite']; // TODO: use limite in request
+router.get('/:id/passages', async (req, res) => {
+    const compteurId = req.params.id;
+    const start = req.query['debut'];
+    const end = req.query['fin'];
+    const limite = req.query['limite']; // TODO: use limite in request
 
-  if (!start || !end) {
-    res.status(400).send({ message: 'Missing start or end date' });
-  }
+    if (!start || !end) {
+        res.status(400).send({message: 'Missing start or end date'});
+    }
 
-  await getCounterModel().findOne({ ID: compteurId }).exec((err, result) => {
-    counter = result;
 
-    const startDate = moment(start).format('YYYY-MM-DDTHH:mm:ss');
-    const endDate = moment(end).add(1, 'days').format('YYYY-MM-DDTHH:mm:ss');
+    await getCounterModel().findOne({ID: compteurId}, {_id: 0, __v: 0}).exec((err, result) => {
 
-    getDataStatsModel().find({ 
-      Date: {
-        $gte: new Date(startDate),
-        $lt: new Date(endDate),
-      },
-    }).sort({ 
-      Date: 'asc',
-    }).exec((err, counters) => {
 
-      // Group and sum counts by Date
-      const counterPerDay = counters.map((e) => {
-        return {
-          date: moment(e.Date).format('YYYY-MM-DD'),
-          count: e[compteurId],
-        };
-      });
-      const sum = [];
-      const counterData = counterPerDay.reduce((rv, c) => {
-        if (!rv[c.date]) {
-          rv[c.date] = { date: c.date, count: 0 };
-          sum.push(rv[c.date]);
-        }
-        rv[c.date].count += c.count;
-        return rv;
-      }, {});
-      
-      res.status(200).send({
-        ...counter._doc,
-        counterData,
-      });
+        const counter = result;
+        const startDate = moment(start).format('YYYY-MM-DDTHH:mm:ss');
+        const endDate = moment(end).add(1, 'days').format('YYYY-MM-DDTHH:mm:ss');
+
+        getDataStatsModel().find({
+            Date: {
+                $gte: new Date(startDate),
+                $lt: new Date(endDate),
+            },
+        },).sort({
+            Date: 'asc',
+        }).exec((err, counters) => {
+
+            // Group and sum counts by Date
+            const counterPerDay = counters.map((e) => {
+                return {
+                    date: moment(e.Date).format('YYYY-MM-DD'),
+                    count: e[compteurId],
+                };
+            });
+
+            const sum = [];
+            const counterData = Object.values(counterPerDay.reduce((rv, c) => {
+                if (!rv[c.date]) {
+                    rv[c.date] = {date: c.date, count: 0};
+                    sum.push(rv[c.date]);
+                }
+                rv[c.date].count += c.count;
+
+                return rv;
+            }, {})).slice(0, limite);
+
+            res.status(200).send({
+                ...counter._doc,
+                counterData,
+            });
+        });
     });
-  });
 });
 
 module.exports = router;
