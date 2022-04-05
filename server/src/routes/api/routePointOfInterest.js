701c1570037e4ceaadbe43cb84f7509ebd82aba4
Ryuketsuki <jimmy.mai.1@ens.etsmtl.ca>
2022-04-05 17:23:05 -0400

    Fix typo

--- server/src/routes/api/routePointOfInterest.js
+++ server/src/routes/api/routePointOfInterest.js
@@ -40,7 +40,7 @@ router.get('/:id', async (req, res) => {
   };
   // build mongoose query here
 
-  getPointOfInterestModel()
+  getPointOfIntrestModel()
     .findOne(query)
     .exec((err, result) => {
       res.status(200).send(result);
